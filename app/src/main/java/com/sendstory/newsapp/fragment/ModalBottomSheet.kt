54eb06169c61c045050912294bd329957b1e64f0
Rajesh Savaliya <rajeshsavaliya143@gmail.com>
2022-05-18 15:59:01 +0530

    Deep linking done

--- app/src/main/java/com/sendstory/newsapp/fragment/ModalBottomSheet.kt
+++ app/src/main/java/com/sendstory/newsapp/fragment/ModalBottomSheet.kt
@@ -8,6 +8,7 @@ import android.os.Build
 import android.os.Bundle
 import android.text.Html
 import android.util.DisplayMetrics
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -33,7 +34,11 @@ class ModalBottomSheet : BottomSheetDialogFragment() {
     lateinit var binding: ModalBottomSheetContentBinding
     lateinit var dialog: BottomSheetDialog
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
         binding = ModalBottomSheetContentBinding.inflate(inflater, container, false)
 
         val data = arguments?.getSerializable(Constants.news) as News
@@ -58,10 +63,20 @@ class ModalBottomSheet : BottomSheetDialogFragment() {
 
         dialog.setOnShowListener { dialogInterface ->
             val bottomSheetDialog = dialogInterface as BottomSheetDialog
-            val bottomSheet = bottomSheetDialog.findViewById<FrameLayout>(com.google.android.material.R.id.design_bottom_sheet) ?: return@setOnShowListener
+            val bottomSheet =
+                bottomSheetDialog.findViewById<FrameLayout>(com.google.android.material.R.id.design_bottom_sheet)
+                    ?: return@setOnShowListener
 
-            bottomSheet.setBackgroundColor(ContextCompat.getColor(requireActivity().applicationContext, android.R.color.transparent))
-            bottomSheet.background = ContextCompat.getDrawable(requireActivity().applicationContext, R.drawable.rounded_dialog)
+            bottomSheet.setBackgroundColor(
+                ContextCompat.getColor(
+                    requireActivity().applicationContext,
+                    android.R.color.transparent
+                )
+            )
+            bottomSheet.background = ContextCompat.getDrawable(
+                requireActivity().applicationContext,
+                R.drawable.rounded_dialog
+            )
 
             bottomSheet.apply {
                 val maxDesiredHeight =
@@ -89,11 +104,16 @@ class ModalBottomSheet : BottomSheetDialogFragment() {
         binding.tvMain.text = news.headline
         binding.tvChName.text = news.publisher!!.pubname
         binding.tvTime.text = hours.toString() + "h"
-        binding.tvSummaryO.text = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            Html.fromHtml(news.formattedSummary, Html.FROM_HTML_MODE_COMPACT)
-        } else {
-            Html.fromHtml(news.formattedSummary)
-        }
+        Log.e(TAG, "showNewsDialog: ${news.formattedSummary}", )
+        val formatted = "<html><head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1, user-scalable=no\" /><style type=\"text/css\">body{font-family: '-apple-system','HelveticaNeue';} img{max-width:100%%; padding:0px; margin:0px;} div.newscontent h1{margin-top:18px; margin-bottom:18px; line-height:30px; font-size:25px; color:black;} div.newscontent h2{margin-top:15px; margin-bottom:15px; line-height:24px; font-size:16px; color:black;} div.newscontent h3{margin-top:25px; margin-bottom:25px; line-height:32px; font-size:17px; color:black;} div.newscontent blockquote{margin:15px; border-left:3px solid #eeeeee; color:#666666; font-size:15px; padding-left:25px;} div.newscontent{font-size:16px; line-height:23px; background-color:white;} div.newscontent a{color:#1761F9;} div.newscontent a:hover{color:#666666;} div.newscontent figcaption{color:#888888; font-size:13px; text-align:center;} iframe{background-color:#f9f9f9;} ul{margin:0px; list-style-type: square; padding-left:16px;} li{color:#1761F9; margin-left:0px; padding:0px; padding-left:10px; margin-bottom:8px;} li span{color:black;}</style></head><body style=\"margin:0px;\"><div align=\"center\"><div style=\"max-width:680px; margin:15px;\" align=\"left\" class=\"newscontent\"><h2>Summary:</h2>" + news.formattedSummary + "</div></div></body></html>"
+//        binding.tvSummaryO.text = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+//            Html.fromHtml(htmlString,
+//                Html.FROM_HTML_MODE_COMPACT
+//            )
+//        } else {
+//            Html.fromHtml(htmlString)
+//        }
+        binding.tvSummaryO.loadDataWithBaseURL("",formatted,"text/html","UTF-8","")
         Picasso.get().load(news.publisher.favicon).into(binding.ivChLogo)
     }
 
